f5eef4a445b088850cba8f7e97817d4a09f36e5a
sindreka <sindre_e@hotmail.com>
2015-09-17 10:57:07 +0200

    Not a good solution, but it works

---
+++ p021.py
@@ -0,0 +1,15 @@
+def sumOfDevisors(num):
+    sum = 0
+    for i in range(1,num):
+        if not num % i:
+            sum += i
+    return sum
+
+totSum = 0
+for i in range(1,10000):
+    su = sumOfDevisors(i)
+    if i == sumOfDevisors(su) and i != su:
+        print("i: ", i, "su", su)
+        totSum += i
+
+print(totSum)
