d328cb153b46d4167649980579c619bf47784b53
sindreka <sindre_e@hotmail.com>
2015-09-19 14:03:57 +0200

    This is not finished

---
+++ p026.py
@@ -0,0 +1,13 @@
+import math
+maxPeriod = 0
+siz = 10**16
+period = -1
+for i in range(3,10):
+    print("siz/i: ", siz/i, "len(str(siz/i))", len(str(siz/i)))
+    print(str(round(siz/i)))
+    #i = -1
+    #while str(round(siz/i))[i:]:
+
+
+
+print(maxPeriod)
