590067f5d899a24cd112535543e317d0f02f8227
Sindre Eskeland <sindre_e@hotmail.com>
2015-09-22 16:37:04 +0200

    I have no longer cheated on this problem

--- p005.py
+++ p005.py
@@ -1,3 +1,19 @@
-print(1*3*4*5*7*2*3)
+def isPrime(num):
+	for i in range(2,round(num**0.5+0.6)):
+		if num % i == 0:
+			return 0
+	return 1
 
-print(1*3*4*5*7*2*3*11*13*2*17*19)
+num = 20
+li = []
+for i in range(2,num):
+	if isPrime(i):
+		li.append(i)
+
+minProd = 1
+for element in li:
+	exponent = 1
+	while element**exponent <= num:
+		minProd *= element
+		exponent += 1
+print(minProd)
