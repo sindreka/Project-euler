5ed24e367238460b53ff93eeb3d3544c01c28edf
Sindre Eskeland <sindre_e@hotmail.com>
2015-09-22 17:10:59 +0200

    Woow, I did it

--- p026.py
+++ p026.py
@@ -1,13 +1,28 @@
-import math
+def getPeriod(siz,num):
+	from decimal import getcontext
+	from decimal import Decimal
+	getcontext().prec = 10000
+	li = (str(round(Decimal(siz)/Decimal(num))))
+	li = li[:-5]
+	le = len(li)
+	period = 1
+	for i in reversed(range(len(li))):
+		if li[i-1] == li[-1]:
+			for j in range(len(li[i:])):
+				if li[i:][j] != li[2*i-le:i][j]:
+					period += 1
+					break
+			else:
+				return period
+		else:
+			period += 1
+
 maxPeriod = 0
-siz = 10**16
+siz = 10**10000
 period = -1
-for i in range(3,10):
-    print("siz/i: ", siz/i, "len(str(siz/i))", len(str(siz/i)))
-    print(str(round(siz/i)))
-    #i = -1
-    #while str(round(siz/i))[i:]:
-
-
-
-print(maxPeriod)
+for i in range(3,1000):
+	period = getPeriod(siz,i)
+	if maxPeriod < period:
+		maxPeriod = period
+		maxNum = i
+print(maxNum)
