ef4d5ff20168c83515277acbeca10fbb0fb812f8
Sindre Eskeland <sindre_e@hotmail.com>
2015-10-05 16:48:23 +0200

    Wohow, finally

--- p031.py
+++ p031.py
@@ -6,36 +6,26 @@ def listMult(li1,li2):
 def numComb(coins,su):
 	li = [0 for i in range(len(coins))]
 	count = 0
-	current = len(coins)-1
+	current = len(coins) - 1
 	n = 0
-	while li[0] < su and n < 10:
-		n += 1
-		for i in reversed(range(current+1)):
+	while li[0] < su:
+		for i in reversed(range(current + 1)):
 			while su >= listMult(coins,li) + coins[i]:
 				li[i] += 1
 		count += 1
-		print("count: ", count ,"li: ", li)
-		#count += 1
-		if current > 0 and  li[current] > 0:
-			li[current] -= 1
-			current -= 1 
-		elif current == 1:# or (len(li)-1 > current  and  li[current+1] == 0):
-			li[current+1] = 0
-			#current += 1
-	#	else: 
-#			current -= 1
+		current = 0
+		while li[current+1] == 0 and current < 6 :
+			current += 1
+		if li[current +1] > 0:
+			li[current+1] -= 1
+		else:
+			break
 		li[0] = 0
 	return count
 
 coins = [1,2,5,10,20,50,100,200]
 su = 200
 
-#numCoins = [0,0,0,0,0,0,0,0]
 
 print(numComb(coins,su))
-
-
-#print(listMult(coins,numCoins))
-
-
 
